2e2d43528cee1e121c74e6fdd1ef31aad4abd575
Hari Sachdeva <hsachdevaucd@gmail.com>
2020-04-28 09:44:35 -0700

    Add APIUtil and Session Action

--- frontend/yelpit.jsx
+++ frontend/yelpit.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import {login} from './util/session_api_util' // Testing
 
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   ReactDOM.render(<h1>Welcome to Yelpit!!!</h1>, root);
   
+  // Testing
+  // window.login = login;
 });
