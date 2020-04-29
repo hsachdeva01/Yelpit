e0df587806201381b4041683c1269b255e933aa9
Hari Sachdeva <hsachdevaucd@gmail.com>
2020-04-28 18:54:28 -0700

    Add Login Page

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import HeaderContainer from './header/header_container';
 import { Route } from 'react-router-dom';
+import LogInFormContainer from './session_form/login_form_container';
+// import SignUpFromContainer from './session_form/signup_form_container';
 
 
 const App = () => (
   <div>
     <h1> Yelpit!!! </h1>
     <Route exact path="/" component={HeaderContainer} />
+    <Route exact path="/login" component={LogInFormContainer} />
+
   </div>
 );
 
--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -1,29 +1,75 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 class SessionForm extends React.Component{
   constructor(props){
     super(props);
-    this.props.formType === 'Sign Up' ? 
-      this.state = {
-        first_name = '',
-        last_name = '',
-        email: '',
-        zip_code: '',
-        birthday: '',
-        password: ''
-      } :
+    // this.props.formType === 'Sign Up' ? 
+    //   this.state = {
+    //     first_name = '',
+    //     last_name = '',
+    //     email: '',
+    //     zip_code: '',
+    //     birthday: '',
+    //     password: ''
+    //   } :
       this.state = {
         email: '',
         password: ''
       };
   }
+  
+  update(field){
+    return e => this.setState({
+      [field]: e.currentTarget.value
+    });
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    const user = Object.assign({}, this.state);
+    this.props.processForm(user);
+  }
 
   render(){
-    return(
-      <div>
-        Session Form
+    
+    const existingUser = () => (
+      <div className="existing-user-form">
+        <div className="header">
+          <a href="/" id="text">Yelpit</a> 
+        </div>
+
+        <div className="session-form">
+          <p className="Login Text">Log in to Yelpit</p>
+          <p className="sign-up-notification">New to Yelpit? <p className="sign-up-text-link"><Link to="/signup">Sign up</Link></p></p>
+          <p className="term-policy-text">By logging in, you agree to Yelpit's Terms and Conditions</p>
+
+          <div className="login-form">
+            <input type="text"
+            value={this.state.email}
+            onChange={this.update('email')}
+            placeholder="Email"
+            required
+            />
+            <br/>
+            <input type="password"
+            value={this.state.password}
+            onChange={this.update('password')}
+            placeholder="Password"
+            required
+            />
+            <button className="login-btn" onClick={this.handleSubmit}>{this.props.formType}</button>
+          </div>
+        </div> 
+
       </div>
+    )
+    
+    
+    
+    return(
+      existingUser()
     )
   }
 };
