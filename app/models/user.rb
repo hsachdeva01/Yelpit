5cfa5261c5b09fe6d1a5612b5ee146a0c62be990
Hari Sachdeva <hsachdevaucd@gmail.com>
2020-04-28 17:36:08 -0700

    Fix argumentError

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 
   validates :first_name, :last_name, :email, :zip_code, :password_digest, :session_token, presence: true
-  validates :email, :session_token, unqueness: true
+  validates :email, :session_token, uniqueness: true
   validates :password, length: { minimum: 6 }, allow_nil: true
 
 
