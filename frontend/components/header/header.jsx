a55f80d013947e3d03f5a5b52ef4a74c298c89f9
Hari Sachdeva <hsachdevaucd@gmail.com>
2020-05-08 12:33:59 -0700

    Add Searchbar

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -1,44 +1,65 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import HomePageDisplay from './homepage_display_pictures'
 import Footer from '../footer/footer';
 
 class Header extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = { search: "", location: ""}
+    // console.log(this.props)
+    this.state = { search: "", location: "" }
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleOnClick = this.handleOnClick.bind(this);
+    //this.testing = this.testing.bind(this);
   }
 
-  handleSubmit(e){
+  handleSubmit(e) {
     e.preventDefault();
-    this.props.SearchBusinesses(this.state.query)
-      .then( () => this.props.history.push("/businesses"))
+    //console.log("Searching");
+    let redirect = "";
+
+    if (this.state.search && this.state.location) {
+      redirect = `/businesses/search/details?keyword=${this.state.search}&location=${this.state.location}`;
+    } else if (this.state.search) {
+      redirect = `/businesses/search/details?keyword=${this.state.search}`;
+    } else if (this.state.location) {
+      redirect = `/businesses/search/details?location=${this.state.location}`;
+    } else {
+      redirect = `/businesses`;
+    }
+
+    this.props.history.push(redirect);
+
   }
 
-  update(field){
+  handleOnClick(e) {
+    e.preventDefault();
+    console.log("Hey")
+  }
+
+  update(field) {
     return (e) => {
-      this.setState({ [field]: e.target.value})
+      this.setState({ [field]: e.target.value })
     }
   }
 
 
-  render(){
+  render() {
     const header = (this.props.currentUser) ? (
       <div className="header-right-logout">
         <button className="logout" onClick={this.props.logout}>Log Out</button>
         <div className="user"><p>Hello, {"  "} {this.props.currentUser.first_name}</p></div>
       </div>
     ) : (
-      <div className="header-right">
-        <p className="login">
-          <Link to="/login">Log In</Link>
-        </p>
-        <p className="signup">
-           <Link to="/signup">Sign Up</Link>
-        </p>
-      </div>
-    );
+        <div className="header-right">
+          <p className="login">
+            <Link to="/login">Log In</Link>
+          </p>
+          <p className="signup">
+            <Link to="/signup">Sign Up</Link>
+          </p>
+        </div>
+      );
     return (
       <div className="homepage">
         <div className="header-mainpage">
@@ -57,8 +78,8 @@ class Header extends React.Component {
             </p>
           </div>
           <div className="main">
-          <div className="application-name">
-            <Link to="/">Yelpit</Link>
+            <div className="application-name">
+              <Link to="/">Yelpit</Link>
             </div>
             <form className="homepage-search">
               <label className="search-find"> Find
@@ -80,16 +101,17 @@ class Header extends React.Component {
                 />
               </label>
               <label className="search-icon" >
-                <Link to="/businesses"> <i className="fas fa-search" /> </Link>
+                {/* <Link to={`/businesses/search/details?keyword=${this.state.search}`}> <i className="fas fa-search" /> </Link> */}
+                <button onClick={this.handleSubmit.bind(this)}> <i className="fas fa-search" /> </button>
               </label>
-          <div className="under-search-bar">
+              <div className="under-search-bar">
                 <li id="accountant"><Link to="/businesses">Accountants</Link></li>
                 <li id="restaurant"><Link to="/businesses">Restaurants</Link></li>
                 <li id="home-service"><Link to="/businesses">Home-Service</Link></li>
                 <li id="delivery"><Link to="/businesses">Delivery</Link></li>
-          </div>
+              </div>
             </form>
-         
+
 
           </div>
         </div>
