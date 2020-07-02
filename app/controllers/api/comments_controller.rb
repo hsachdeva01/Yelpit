3542c4834a0d8c45d4e4c9973bfda915ad2a82ba
Hari Sachdeva <hsachdevaucd@gmail.com>
2020-07-02 15:41:36 -0700

    Update controller and db for comments

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,37 @@
+class Api::CommentsController < ApplicationController
+
+  def index
+    @comments = Comment.all
+    render :index
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      render :info
+    else 
+      render json: @comment.errors.full_messages, status: 422
+    end
+  end
+
+  def show
+    @comment = Comment.find(params[:id])
+    render :info
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    if @comment.destroy
+      render :info
+    else
+      render json: @comment.errors.full_messages, status: 422
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:user_id, :review_id, :content)
+  end
+
+end
