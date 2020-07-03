16cb72d4450119c7b4e32cc90f7bbf7c13a912b1
Hari Sachdeva <hsachdevaucd@gmail.com>
2020-07-03 14:52:34 -0700

    Add Comments util

---
+++ frontend/util/comments_api_util.js
@@ -0,0 +1,22 @@
+export const fetchComments = () => (
+  $.ajax({
+    method: 'GET',
+    url: 'api/comments'
+  })
+);
+
+export const createComment = comment => (
+  $.ajax({
+    method: 'POST',
+    url: 'api/comments',
+    data: {comment}
+  })
+);
+
+export const deleteComment = id => (
+  $.ajax({
+    method: 'DELETE',
+    url: `/api/comments/${id}`,
+    data: id
+  })
+);
