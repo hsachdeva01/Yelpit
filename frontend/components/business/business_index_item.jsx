b97ba4b0c2972f4cb0b15193070f02416e50a655
Hari Sachdeva <hsachdevaucd@gmail.com>
2020-08-04 16:13:23 -0700

    Remove BusinessIndex from BusinessIndexItem

--- frontend/components/business/business_index_item.jsx
+++ frontend/components/business/business_index_item.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import BusinessIndex from './business_index';
 
 const BusinessIndexItem = props => (
   <div className="business-index-item">
