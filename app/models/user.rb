743ea94706ee308575cba67e6d5e4115f3d43af2
Hari Sachdeva <hsachdevaucd@gmail.com>
2022-03-31 17:12:10 -0700

    testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,5 @@
 class User < ApplicationRecord
 
-  validates :first_name, :last_name, :email, :zip_code, :password_digest, :session_token, presence: true
-  validates :email, :session_token, uniqueness: true
-  validates :password, length: { minimum: 6 }, allow_nil: true
-
-  has_many :reviews,
-  primary_key: :id,
-  foreign_key: :author_id,
-  class_name: :Review
 
   has_many :comments,
   primary_key: :id,
